ce408ef8c3b820f1a5c97058c98df94e857746f3
l3miage-monachov <155984981+l3miage-monachov@users.noreply.github.com>
2024-10-09 14:45:47 +0200

    "project skeleton"

---
+++ src/main/java/fr/uga/l3miage/pc/api/endpoints/GameEndpoints.java
@@ -0,0 +1,9 @@
+package fr.uga.l3miage.pc.api.endpoints;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1.0/game")
+public interface GameEndpoints {
+}
